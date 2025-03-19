8bce0305f3699e944893c8b0290fb4a17bc5c270
Daniela Nogueira Rampim <90867586+nogueiraDani@users.noreply.github.com>
2025-03-19 08:03:03 -0300

    aula 14-03-25

---
+++ Aula01/src/aula_14_03_25/Ex02.java
@@ -0,0 +1,27 @@
+package aula_14_03_25;
+
+import java.util.Scanner;
+
+public class Ex02 {
+
+	public static void main(String[] args) {
+
+		/**
+		 * Dados dois valores inteiros, escreva um algoritmo que informe se o primeiro
+		 * número é múltiplo do segundo
+		 */
+
+		PedirNumero pedirNumero = new PedirNumero();
+
+		int num1 = pedirNumero.perguntar();
+		int num2 = pedirNumero.perguntar();
+
+		/*
+		 * if (num1 % num2 == 0) { System.out.println("Verdadeiro"); } else {
+		 * System.out.println("False"); }
+		 */
+
+		System.out.println(num1 % num2 == 0);
+	}
+
+}
---
+++ Aula01/src/aula_14_03_25/PedirNumero.java
@@ -0,0 +1,20 @@
+package aula_14_03_25;
+
+import java.util.Scanner;
+
+public class PedirNumero {
+
+	int numero;
+	
+	Scanner scanner = new Scanner(System.in);
+	
+		
+	public PedirNumero() {
+	}
+
+	public int perguntar() {
+		System.out.println("Digite um numero:");
+		return scanner.nextInt();
+	}
+
+}
