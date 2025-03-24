5549cb18b481b0af7703515ee45ae7e447698571
Daniela Nogueira Rampim <90867586+nogueiraDani@users.noreply.github.com>
2025-03-24 10:17:39 -0300

    aula 19-03-25

---
+++ Aula01/src/aula_19_03_25/Ex15.java
@@ -0,0 +1,57 @@
+package aula_19_03_25;
+
+import java.util.Scanner;
+
+public class Ex15 {
+ 
+    public static void main(String[] args) {
+ 
+        /**
+         * O índice de massa corporal (IMC) é uma medida internacional usada para
+         * calcular se uma pessoa está no peso ideal.
+         * O IMC é determinado pela divisão da massa do indivíduo pelo
+         * quadrado de sua altura, onde a massa está em quilogramas e a altura está em
+         * metros, de acordo com a fórmula:
+         * IMC = Massa / Altura²
+         * Faça um algoritmo para classificar o IMC e dizer o grau de obesidade do
+         * indivíduo, de acordo com a seguinte tabela:
+         * < 18.5 Magreza
+         * 18.5 – 24.9 Saudável
+         * 25.0 – 29.9 Sobrepeso
+         * 30.0 – 34.9 Obesidade Grau I
+         * 35.0 – 39.9 Obesidade Grau II (severa)
+         * > 40.0 Obesidade Grau III (morbida)
+         */
+ 
+        Scanner scanner = new Scanner(System.in);
+ 
+        double peso;
+        double altura;
+ 
+        System.out.println("Digite o peso:");
+        peso = scanner.nextDouble();
+ 
+        System.out.println("Digite a altura:");
+        altura = scanner.nextDouble();
+ 
+        double imc = peso / Math.pow(altura, 2);
+ 
+        System.out.print("Classificação: ");
+ 
+        if (imc < 18.5) {
+            System.out.print("magreza.");
+        } else if (imc >= 18.5 && imc <= 24.9) {
+            System.out.print("saudável.");
+        } else if (imc >= 25 && imc <= 29.9) {
+            System.out.print("sobrepeso.");
+        } else if (imc >= 30 && imc <= 34.9) {
+            System.out.print("obesidade grau I.");
+        } else if (imc >= 35 && imc <= 39.9) {
+            System.out.print("obesidade grau II (severa).");
+        } else {
+            System.out.print("obesidade grau III (morbida).");
+        }
+ 
+    }
+ 
+}
---
+++ Aula01/src/aula_21_03_25/Ex19.java
@@ -0,0 +1,46 @@
+package aula_21_03_25;
+
+import java.util.Scanner;
+
+public class Ex19 {
+ 
+    public static void main(String[] args) {
+ 
+        /**
+         * A jornada de trabalho semanal de um funcionário é de 40 horas. O funcionário
+         * que trabalhar mais de 40 horas receberá hora extra, cujo cálculo é o valor da
+         * hora regular
+         * com um acréscimo de 50%. Escreva um algoritmo que leia o número de horas
+         * trabalhadas em um
+         * mês, o valor por hora e escreva o salário total do funcionário, que deverá
+         * ser acrescido das
+         * horas extras, caso tenham sido trabalhadas (considere que o mês possua 4
+         * semanas exatas)
+         */
+ 
+        Scanner scanner = new Scanner(System.in);
+ 
+        double horasSemanaisPadrao = 40;
+        double horasMensaisPadrao = horasSemanaisPadrao * 4;
+        double valorPorHora,salario, horasTrabalhadasMes, valorHoraExtra, horasExtrasTrabalhadas;
+        double adicionalHoraExtra = 0.5;
+       
+        System.out.println("Digite o numero de horas trabalhadas no mês:");
+        horasTrabalhadasMes = scanner.nextDouble();
+ 
+        System.out.println("Digite o valor da hora trabalhada:");
+        valorPorHora = scanner.nextDouble();
+ 
+        valorHoraExtra = valorPorHora + (valorPorHora * adicionalHoraExtra);
+ 
+        if (horasTrabalhadasMes > 160) {
+            horasExtrasTrabalhadas = horasTrabalhadasMes - horasMensaisPadrao;
+            salario = (horasMensaisPadrao * valorPorHora) + (horasExtrasTrabalhadas * valorHoraExtra);
+        } else {
+            salario = (horasTrabalhadasMes * valorPorHora);
+        }
+ 
+        System.out.printf("o salario total é R$ %.2f", salario);
+ 
+    }
+}
