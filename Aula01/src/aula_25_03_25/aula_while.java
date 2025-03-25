5b77754b9c52237e7bd4195ee63f9cc25470262f
Daniela Nogueira Rampim <90867586+nogueiraDani@users.noreply.github.com>
2025-03-25 11:28:28 -0300

    aula 25-03-25

---
+++ Aula01/src/aula_25_03_25/aula_while.java
@@ -0,0 +1,37 @@
+package aula_25_03_25;
+
+import java.util.Scanner;
+
+public class aula_while {
+
+	public static void main(String[] args) {
+
+		/**
+		 * Faça um algoritmo que escreve do 20 ao 10
+		 */
+
+		Scanner scanner = new Scanner(System.in);
+
+		int valorInicial, valorFinal;
+
+		System.out.println("Digite o valor inicial:");
+		valorInicial = scanner.nextInt();
+
+		System.out.printf("Digite o valor final diferente de %d:\n", valorInicial);
+		valorFinal = scanner.nextInt();
+
+		if (valorInicial > valorFinal) {
+			while (valorInicial >= valorFinal) {
+				System.out.println(valorInicial);
+				valorInicial--;
+			}
+		} else {
+			while (valorInicial <= valorFinal) {
+				System.out.println(valorInicial);
+				valorInicial++;
+			}
+		}
+
+	}
+
+}
